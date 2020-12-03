426fab713166ab9b123d0534b4459134af979708
Udoh Idorenyin <idorenyinudoh@users.noreply.github.com>
2020-12-03 15:01:43 +0100

    finish work

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 const messageBox = document.getElementById('message');
 const latitudeBox = document.getElementById('latitude');
 const longitudeBox = document.getElementById('longitude');
-const timeBox = document.getElementById('time');
+const countryBox = document.getElementById('country');
+const countryParentBox = document.getElementById('country-parent');
 
 if('geolocation' in navigator) {
     navigator.geolocation.watchPosition((position) => {
-        latitudeBox.textContent = position.coords.latitude;
-        longitudeBox.textContent = position.coords.longitude;
-        timeBox.textContent = position.timestamp;
+        const lat = position.coords.latitude;
+        const long = position.coords.longitude;
+
+        latitudeBox.textContent = lat;
+        longitudeBox.textContent = long;
+
+        (async function() {
+            const response = await fetch(`https://nominatim.openstreetmap.org/reverse?format=jsonv2&zoom=3&lat=${lat}&lon=${long}`);
+            return response.json();
+        })()
+        .then(data => {
+            countryBox.textContent = data.address.country + String.fromCodePoint(0x1F609);
+        })
+        .catch(err => {
+            countryParentBox.textContent = 'Unable to get your country. Please make sure you have a good connection.';
+        });
+
+    },
+    error => {
+        messageBox.textContent = 'Unable to get your location. Please make sure you have a good connection.';
     });
 } else {
-    messageBox.textContent = 'Sorry, geolocation is not supported by your browser&#128530;';
+    messageBox.textContent = 'Sorry, geolocation is not supported by your browser' + String.fromCodePoint(0x1F612);
 }
